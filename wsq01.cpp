2188194402298b0378c695af37b81407c92e3ae7
valeriact <30931334+valeriact@users.noreply.github.com>
2017-09-15 08:45:43 -0500

    Create wsq01.cpp

---
+++ wsq01.cpp
@@ -0,0 +1,21 @@
+// Fun with numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Type in 2 values" << endl;
+    int Number1;
+    int Number2;
+    
+    cin >> Number1;
+    cin >> Number2;
+    
+    cout << "Sum:" << Number1 + Number2 << endl;
+    cout << "Difference:" << Number1 - Number2 << endl;
+    cout << "Product:" << Number1 * Number2 << endl;
+    cout << "Integer based division:" << Number1 / Number2 << endl;
+    cout << "Remainder:" << Number1 % Number2 << endl;
+    
+}
