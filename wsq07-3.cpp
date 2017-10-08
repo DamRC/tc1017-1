658966497586faa0bb9f9c37f66db7b3d3d5bdd1
valeriact <valeria.crutam@gmail.com>
2017-10-08 00:28:35 -0500

    advances on wsq07-3

--- wsq07-3.cpp
+++ wsq07-3.cpp
@@ -42,8 +42,7 @@ int main ()
     cout << "when you're done, please write 'I'm done' " << endl;
     // because the input type is a double, any string input will terminate the loop
 
-
-    double list[]; //extern allows the array to have an unknown size
+    extern double list[]; //extern allows the array to have an unknown size
     double x;
     int n = 0;
 
@@ -53,9 +52,11 @@ int main ()
         list [n] = x;
     }
 
-    int m = list.size(list);
+    int m = list.size();
     cout << "The sum of those values is: " << total (0, 0, list, m) << endl; // assigns value "0" to n and result and the array list to the parameter list
     double sum = total (0, 0, list, m);
+    cout << "The sum of those values is: " << total (0, 0, list) << endl; // assigns value "0" to n and result and the array list to the parameter list
+    double sum = total (0, 0, list);
     cout << "The average of the values is: " << average (sum, 0, m) << endl; // this gives the sum in the previous function as parameter for the average
     double avg = average(sum, 0, m);
     cout << "The standard deviation of the values provided is: " << standarddeviation (0, 0, 0, sum, avg, list, 0, m) << endl;
