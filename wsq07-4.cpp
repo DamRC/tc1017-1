394e9bef56a09e638efc547b38c1ae175ce615dd
valeriact <valeria.crutam@gmail.com>
2017-10-11 14:02:37 -0500

    start vectors
    intento de entender strings y vectors

--- wsq07-4.cpp
+++ wsq07-4.cpp
@@ -1,7 +1,41 @@
+// Vectors
 #include <iostream>
 #include <cmath>
-#include <vector>
+#include <vector> //include vector library
+#include <string>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::vector;
+
+int main ()
+{
+      cout << "Please type in as many values as you want (press enter after each): " << endl;
+      cout << "When you're done, write 'no more values'" << endl;
+      vector<double> list; // initialization of vector
+      double x;
+      string done;
+
+      for ()
+
+      int n = 0;
+
+      for ()
+      {
+          cin >> x;
+          if (cin.fail()){
+            if (x=done){
+              if done = "no more values"{
+                return 0;
+              } el
+            }
+          }
+          do{
+
+          } while (!cin.fail())
+          list.push_back (x);
+      }
+
+
+  return 0;
+}
