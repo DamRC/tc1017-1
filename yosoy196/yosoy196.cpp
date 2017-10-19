b17bf5534f300f8f3e5f619ed8eaf5a5934ac199
valeriact <valeria.crutam@gmail.com>
2017-10-19 16:30:57 -0500

    trial
    still needs work

--- yosoy196/yosoy196.cpp
+++ yosoy196/yosoy196.cpp
@@ -17,35 +17,62 @@ using namespace std;
  * This function receives a BigInteger n and checks if it is a palindrome
  * Return true if n is a palindrome, return false otherwise
  */
-bool is_palindrome(BigInteger n){
-  string num;
-  num = bigIntegerToString(n);
-  int lenght = num.legth();
+bool is_palindrome(string num, int length, int back){
 
-
+  for (int i=0; i<(length/2); i++){
+    int b = back - i;
+    if (num[i] != num[b]){
+      return false;
+    }
+  }
   return true; // You need to fix this, this is not right
 }
 
 /*
- * This function receives a BigInteger n and returns the value of n plus the inverse of n.
+ * This function receives a string n and returns a BigInteger sum (new value to check palindromicity).
  */
-BigInteger apply196(BigInteger n){
-  return n; // you need to replace this with code that works
+BigInteger Iterate(string num, int back, BigInteger numint){
+  string inverse;
+  for (int i=0; i<=back; i++){
+    int b = back - i;
+    inverse[i] = num[b];
+    inverse = inverse + inverse[i];
+  }
+  BigInteger invint = stringToBigInteger(inverse);
+  BigInteger sum;
+  sum = numint + invint;
+
+  return sum;
 }
 
 /*
  * Your main program goes below and should use the two functions above
  */
 int main() {
-  cout << "Hola mundo" << endl;
-  cout << "Holaaa" << endl;
-  BigInteger n;
-  cout << "type in a number" << endl;
-  getline(cin, n);
   string num;
-  num = bigIntegerToString(n);
-  int length = num.length();
+  BigInteger numint;
+  cout << "type in a number: ";
+  getline(cin, num);
+  numint = stringToBigInteger(num);
 
-  cout << "length: " << length << endl;
+  int length = num.length(); //count so you can compare opposites
+  int back = length - 1;
+
+
+  recurse:
+  if (is_palindrome(num, length, back) == true){
+    cout << num << " is a palindrome" << endl;
+    goto next;
+  }
+  else{
+    cout << num << " is NOT  a palindrome" << endl;
+    BigInteger sum = Iterate(num, back, numint);
+    num = bigIntegerToString(sum);
+    goto recurse;
+  }
+
+  next:
+  cout << "done" << endl; //erase later
+
   return 0;
 }
